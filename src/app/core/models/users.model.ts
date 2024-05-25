b45c4ca52055a26ea4bdd5fa4e82fe97ad92b303
JoaoVitorPerin <joao.vperin@outlook.com>
2024-05-25 13:57:53 -0300

    feat(pagina-home): pesquisando users corretamente

---
+++ src/app/core/models/users.model.ts
@@ -0,0 +1,7 @@
+export interface users {
+    avatar: string,
+    name: string,
+    qtdRepos: number,
+    qtdFollowers: number,
+  }
+  
--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from 'src/app/core/services/user.service';
+import { users } from 'src/app/core/models/users.model';
+import { ToastModule } from 'primeng/toast';
+import { animacaoPadrao, inOutAnimation, inOutAnimationFast } from 'src/app/core/animation';
+import { TruncatePipe } from 'src/app/core/pipes/truncate.pipe';
 
 @Component({
   selector: 'app-search-bar',
@@ -15,17 +19,22 @@ import { UserService } from 'src/app/core/services/user.service';
     ReactiveFormsModule,
     InputGroupModule,
     InputGroupAddonModule,
-    ButtonModule
+    ButtonModule,
+    ToastModule,
+    TruncatePipe
   ],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  styleUrl: './search-bar.component.css',
+  animations: [animacaoPadrao, inOutAnimation, inOutAnimationFast],
 })
 export class SearchBarComponent {
   searchForm: FormGroup;
+  usersSearched: users[] = [];
+  userNotFound = false;
 
   constructor(
     private formBuilder: FormBuilder,
-    private userService: UserService
+    private userService: UserService,
   ){
     this.searchForm = formBuilder.group({
       usernameSearch: ['', Validators.required]
@@ -36,7 +45,20 @@ export class SearchBarComponent {
     this.searchForm.markAllAsTouched();
     if(this.searchForm.valid){
       this.userService.searchUsers(this.searchForm.value.usernameSearch).subscribe((response) => {
-        console.log(response);
+        if(response.items.length === 0){
+          this.userNotFound = true;
+          this.usersSearched = [];
+          return;
+        }
+        this.userNotFound = false;
+        this.usersSearched = response.items.map((user: any) => {
+          return {
+            avatar: user.avatar_url,
+            name: user.login,
+            qtdRepos: user.public_repos?.length,
+            qtdFollowers: user.followers_url?.length
+          };
+        });
       },
       (error) => {
         console.error(error);
